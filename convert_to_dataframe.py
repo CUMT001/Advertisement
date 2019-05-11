7f17bcf71905cd17e8e75e4f69e17b8228077fd2
CUMT001 <304476822@qq.com>
2019-05-11 15:52:40 +0800

    solve the Nan problem

--- convert_to_dataframe.py
+++ convert_to_dataframe.py
@@ -2,8 +2,13 @@ import pandas as pd
 import numpy as np
 
 
+def deal_comma():
+    pass
+
+
 dict_data = {}
-with open("E:/algo.qq.com_641013010_testa/testA/ad_static_feature.out", "r") as fh:
+# 在不同机器上运行时需要改变路径
+with open("H:/tencent_ad/ad_static_feature.out", "r") as fh:
     # 字典中的键列表，比如这里分别代表广告 id、创建时间、广告账户 id、商品id、商品类型、广告行业id、素材尺寸
     key_list = ['id', 'time', 'account', 'product', 'product_type', 'trade', 'size']
     # 获得每行的长度
@@ -11,12 +16,13 @@ with open("E:/algo.qq.com_641013010_testa/testA/ad_static_feature.out", "r") as 
     for line in fh:
         data = line.strip().split('\t')
         for i in range(cnt):
-            if cnt ==len(data):
-                dict_data.setdefault(key_list[i], []).append(data[i])
-            else:
+            if i >=len(data):
                 dict_data.setdefault(key_list[i], []).append(np.NaN)
+            else:
+                dict_data.setdefault(key_list[i], []).append(data[i])
 
 print(type(dict_data.keys()))
 columnsname = list(dict_data.keys())
 frame = pd.DataFrame(dict_data, columns=columnsname)
-frame.to_csv('file_out_asf.csv', index=False, header=False)
+# 转换不同的文件应该修改保存文件名
+frame.to_csv('file_out_asf.csv', index=False, header=True)
